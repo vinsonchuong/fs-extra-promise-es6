5845aedabdd4868791f63201ee0ba871697d6874
Vinson Chuong <vinsonchuong@gmail.com>
2015-12-30 14:31:24 -0800

    Allow a release under Babel 6

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import {promisify} from 'node-promise-es6';
 import * as fse from 'fs-extra';
 
-export * from 'fs-extra';
+// export * from 'fs-extra';
+/* eslint-disable */
+export const copySync = fse.copySync;
+export const createOutputStream = fse.createOutputStream;
+export const emptyDirSync = fse.emptyDirSync;
+export const ensureFileSync = fse.ensureFileSync;
+export const ensureDirSync = fse.ensureDirSync;
+export const ensureLinkSync = fse.ensureLinkSync;
+export const ensureSymlinkSync = fse.ensureSymlinkSync;
+export const mkdirsSync = fse.mkdirsSync;
+export const outputFileSync = fse.outputFileSync;
+export const outputJsonSync = fse.outputJsonSync;
+export const readJsonSync = fse.readJsonSync;
+export const removeSync = fse.removeSync;
+export const writeJsonSync = fse.writeJsonSync;
+/* eslint-enable */
 
 export const copy = promisify(fse.copy);
 export const emptyDir = promisify(fse.emptyDir);
